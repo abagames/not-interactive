d75c69f73eea92ef2419551b72b924cd6e15377f
ABA Games <cs8k-cyu@asahi-net.or.jp>
2017-07-17 16:00:34 +0900

    Add sthe ample games directory

--- webpack.config.js
+++ webpack.config.js
@@ -1,24 +1,36 @@
-module.exports = {
-  entry: './src/index.ts',
-  output: {
-    path: __dirname + '/docs',
-    filename: 'bundle.js'
-  },
-  resolve: {
-    extensions: ['.ts', '.js'],
-    modules: ['node_modules', 'web_modules']
-  },
-  devtool: 'source-map',
-  devServer: {
-    contentBase: 'docs'
-  },
-  module: {
-    rules: [
-      {
-        test: /\.ts$/,
-        exclude: /(node_modules|web_modules)/,
-        loader: 'awesome-typescript-loader'
-      }
-    ]
+module.exports = function (env) {
+  var config = {
+    resolve: {
+      extensions: ['.ts', '.js'],
+      modules: ['node_modules', 'web_modules']
+    },
+    devServer: {
+      contentBase: 'docs'
+    },
+    module: {
+      rules: [
+        {
+          test: /\.ts$/,
+          exclude: /(node_modules|web_modules)/,
+          loader: 'awesome-typescript-loader'
+        }
+      ]
+    }
+  };
+  if (env == null || env.sample_game == null) {
+    config.entry = './src/index.ts';
+    config.output = {
+      path: __dirname + '/docs',
+      filename: 'bundle.js'
+    };
+    config.devtool = 'source-map';
+  } else {
+    var sampleGame = env.sample_game;
+    config.entry = './src/samples/' + sampleGame + '.ts';
+    config.output = {
+      path: __dirname + '/docs/samples',
+      filename: sampleGame + '.js'
+    };
   }
-};
+  return config;
+}
